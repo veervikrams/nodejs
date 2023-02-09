5f69813f1bb644a0f1ea3456efa71447838542d4
unknown <veervikrams84@gmail.com>
2023-02-10 05:09:58 +0530

    file added

---
+++ t1_let-const.js
@@ -0,0 +1,55 @@
+// Var, Let, const
+
+
+//const
+
+// const number = 10;
+
+// console.log(number);
+
+
+
+
+// function printNumber(){  
+    
+//     for(let i=0;i<10;i++){
+//         console.log(i);
+//     }
+//     console.log("Number outside for loop", i);
+// }
+// printNumber();
+
+
+
+
+
+// function printmarks(){
+//     const marks =  80;
+//     console.log(marks);
+// }
+
+// printmarks();
+
+
+//problems with var
+
+// function printNumber(){
+//     for(var i=0;i<10;i++){
+//         console.log(i);
+//     }
+//     console.log("Number outside for loop", i);
+// }
+// printNumber();
+
+
+// function shouldPrint(){
+//     var print = true;
+//     if(print){
+//         var numberToPrint = 10;
+//         console.log("Printing : ", numberToPrint );
+//     }
+//     console.log(numberToPrint)
+// }
+// shouldPrint()
+
+
